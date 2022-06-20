45d1a39cbaf69c6e57ac08f0e9683f6df461368d
GaleanoJorge <juandavidrianoperez@gmail.com>
2022-06-20 09:08:50 -0500

    estilos y organizaciòn pantalla login

--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -14,50 +14,85 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Container(
         width: double.infinity,
-        child: Column(children: <Widget>[
-          _imageBanner(),
-          _textfieldLogin('Correo Electrónico'),
-          _textfieldLogin('Contraseña'),
-          _botonIngreso(),
-          _textDontHaveAccount(),
-        ]),
+        child: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            _imageBanner(),
+            _textfieldLogin('Correo Electrónico', Icons.email),
+            _textfieldLogin('Contraseña', Icons.lock),
+            _botonIngreso(),
+            _textDontHaveAccount(),
+          ]),
+        ),
       ),
     );
   }
 
   Widget _textDontHaveAccount() {
     return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('¿No tienes cuenta?', style: TextStyle(
-                    color: MyColors.primaryColor,),),
-            SizedBox(
-              width: 7,
-            ),
-            FlatButton(
-              child: Text(
-                'Registrate',
-                style: TextStyle(
-                    color: MyColors.primaryColor,
-                    fontWeight: FontWeight.bold),
-              ),
-              onPressed: () {},
-            )
-          ],
-        );
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          '¿No tienes cuenta?',
+          style: TextStyle(
+            color: MyColors.primaryColor,
+          ),
+        ),
+        SizedBox(
+          width: 7,
+        ),
+        FlatButton(
+          child: Text(
+            'Registrate',
+            style: TextStyle(
+                color: MyColors.primaryColor, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () {},
+        )
+      ],
+    );
   }
 
-  Widget _botonIngreso() => ElevatedButton(onPressed: () {}, child: Text('Ingresar'));
+  Widget _botonIngreso() => Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text('INGRESAR'),
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            padding: EdgeInsets.symmetric(
+              vertical: 15,
+            ),
+          ),
+        ),
+      );
 
-  Widget _textfieldLogin(String hintText) {
-    return TextField(
+  Widget _textfieldLogin(String hintText, IconData iconData) => Container(
+        margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+        decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: TextField(
           decoration: InputDecoration(
             hintText: hintText,
+            hintStyle: TextStyle(
+              color: MyColors.primaryColorDark,
+            ),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(15),
+            prefixIcon: Icon(
+              iconData,
+              color: MyColors.primaryColor,
+            ),
           ),
-        );
-  }
+        ),
+      );
 
-  Widget _imageBanner() {
-    return Image.asset('assets/img/delivery.png', width: 200, height: 200);
-  }
+  Widget _imageBanner() => Container(
+      margin: EdgeInsets.only(
+          top: 100, bottom: MediaQuery.of(context).size.height * 0.22),
+      child: Image.asset('assets/img/delivery.png', width: 200, height: 200));
 }
