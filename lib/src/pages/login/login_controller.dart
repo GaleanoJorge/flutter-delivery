a26a45356d8c83ab96ee9741d539452b6c2592d7
GaleanoJorge <juandavidrianoperez@gmail.com>
2022-07-06 19:55:13 -0500

    metodos de logeo implementados

--- lib/src/pages/login/login_controller.dart
+++ lib/src/pages/login/login_controller.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_delivery/src/models/response_api.dart';
+import 'package:flutter_delivery/src/provider/users_provider.dart';
+import 'package:flutter_delivery/src/utils/my_snackbar.dart';
 
 class LoginController {
-
   BuildContext? context;
 
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  UsersProvider usersProvider = UsersProvider();
+
   Future? init(BuildContext context) async {
     this.context = context;
+    await usersProvider.init(context);
   }
 
   void goToRegisterPage() {
     Navigator.pushNamed(context!, 'register');
   }
 
-  void login() {
+  void login() async {
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
 
-    print('$email $password');
+    ResponseApi? responseApi = await usersProvider.login(email, password);
+    MySnackbar.show(context!, responseApi!.message);
+
+    print('Respuesta: ${responseApi.toJson()}');
   }
 }
